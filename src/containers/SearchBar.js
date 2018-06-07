1bc5f96178e7b8627f4c54974af926b7e5ce9855
Yone <yone.moreno101@alu.ulpgc.es>
2018-06-07 05:37:52 +0100

    We have created a controlled form input because its value is being updated by the inner state of the searchBar component itself

--- src/containers/SearchBar.js
+++ src/containers/SearchBar.js
@@ -1,15 +1,28 @@
 import React, {Component} from 'react';
 
 export default class SearchBar extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
+
+        this.onInputChange = this.onInputChange.bind(this);
 
         this.state = {term: ''};
     }
+
+    onInputChange(e) {
+        console.log(e.target.value);
+        this.setState({term: e.target.value});
+    }
+
     render() {
         return (
             <form className='input-group'>
-                <input/>
+                <input
+                    placeholder='Write a city name to get its forecast!'
+                    className='form-control'
+                    value={this.state.term}
+                    onChange={this.onInputChange}
+                />
                 <span className='input-group-btn'>
                     <button className='btn btn-secondary' type='submit'>Search</button>
                 </span>

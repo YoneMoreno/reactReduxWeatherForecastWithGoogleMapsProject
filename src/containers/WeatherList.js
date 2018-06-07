71bd1aaf819ea5661f15785b654d38d6ba72de1e
Yone <yone.moreno101@alu.ulpgc.es>
2018-06-07 08:33:34 +0100

    We calculate the average temperature and print it under the chart. In addition we draw a reference line showing what the current average is. Plus we convert kelvin temperature to centigrade

--- src/containers/WeatherList.js
+++ src/containers/WeatherList.js
@@ -6,7 +6,8 @@ class WeatherList extends Component {
     renderWeather(cityData) {
 
         const name = cityData.city.name;
-        const temps = cityData.list.map(weather => weather.main.temp);
+        const convertFromKelvinToCentegrees = 273.15;
+        const temps = cityData.list.map(weather => weather.main.temp - convertFromKelvinToCentegrees);
         const pressures = cityData.list.map(weather => weather.main.pressure);
         const humidities = cityData.list.map(weather => weather.main.humidity);
         // console.log(temps);

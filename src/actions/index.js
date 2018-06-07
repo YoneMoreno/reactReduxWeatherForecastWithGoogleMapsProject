b30b3644352fdb7891c09faa16d43e8dc453db6b
Yone <yone.moreno101@alu.ulpgc.es>
2018-06-07 06:48:33 +0100

    Fix: there must be only one export default in the class SearchBar and as it is a Container it should be the one in the connect method.
    Fix: The way we have to make aware this current Container: SearchBar of the actionCreators is to creste the function mapDispatchToProps. In that function we must return the result of binding the wanted actionCreator with the way we have to communicate with all the reducers: dispatch
    
    Fix: ROOT_URL was pointing to samples but it should point to the api itself

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,11 +2,11 @@ import axios from 'axios';
 
 const API_KEY = '210b0c6728e6ae1bd4ac36f5ee37bc20';
 const ROOT_URL =
-    `http://samples.openweathermap.org/data/2.5/forecast?&appid=${API_KEY}`;
+    `http://api.openweathermap.org/data/2.5/forecast?&appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
-export default function fetchWeather(city) {
+export function fetchWeather(city) {
     const url = `${ROOT_URL}&q=${city},es`;
     const request = axios.get(url);
 

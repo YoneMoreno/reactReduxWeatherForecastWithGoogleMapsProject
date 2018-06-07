b5f737613238cd73ae9d84faf2e0edf0553f625b
Yone <yone.moreno101@alu.ulpgc.es>
2018-06-07 06:22:14 +0100

    Adding the action creator fetchWeather. TODO make an AJAX request

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,11 @@
 const API_KEY = '210b0c6728e6ae1bd4ac36f5ee37bc20';
+const ROOT_URL =
+    'http://samples.openweathermap.org/data/2.5/forecast?&appid=';
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export default function () {
+    return {
+        type: FETCH_WEATHER
+    }
+}

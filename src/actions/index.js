185cd8eccd498cff9f80d12b39945d61da0964f4
Yone <yone.moreno101@alu.ulpgc.es>
2018-06-07 06:30:28 +0100

    We use axios library to fetch data through an AJAX request

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,11 +1,17 @@
+import axios from 'axios';
+
 const API_KEY = '210b0c6728e6ae1bd4ac36f5ee37bc20';
 const ROOT_URL =
-    'http://samples.openweathermap.org/data/2.5/forecast?&appid=';
+    `http://samples.openweathermap.org/data/2.5/forecast?&appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
-export default function () {
+export default function fetchWeather(city) {
+    const url = `${ROOT_URL}&q=${city},es`;
+    const request = axios.get(url);
+
     return {
-        type: FETCH_WEATHER
+        type: FETCH_WEATHER,
+        payload: request
     }
 }
